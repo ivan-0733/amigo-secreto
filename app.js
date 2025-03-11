7b38a5ba92662bdcdcf7a061afc771efc5b2ac0a
Ivan Morales <ivanmoralesolivares@gmail.com>
2025-03-10 22:00:28 -0600

    Actualizar el app

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let nombres = []
-let mensajeDeError = ["¡Error! No puedes ingresar un nombre en blanco. Ingresa un nombre válido", "¡Error! Nombre ya ingresado. Ingresar un nuevo nombre"]
+let mensajeDeError = ["¡Error! No puedes ingresar un nombre en blanco. Ingresa un nombre válido", "¡Error! Nombre ya ingresado. Ingresar un nuevo nombre", "¡Error! Debes ingresar por lo menos 2 nombres"]
 
 function agregarAmigo(){
     let nombre = document.getElementById("amigo").value;
@@ -31,4 +31,24 @@ function mostrarNombres(){
     for (let i = 0; i < nombres.length; i++) {
         listaAmigos.innerText += nombres[i] + "\n";
     }
+}
+
+function elegirAmigo(){
+    if (nombres.length < 2){
+        alert(mensajeDeError[2])
+        return;
+    }
+
+    let posicionAmigoAleatorio = Math.floor(Math.random() * nombres.length);
+
+    return posicionAmigoAleatorio;
+}
+
+function sortearAmigo(){
+    let amigoSecreto = document.getElementById("resultado");
+    let indice = elegirAmigo();
+
+    amigoSecreto.innerText = "";
+
+    amigoSecreto.innerText = `Tu amigo secreto es: ${nombres[indice]}`;
 }
